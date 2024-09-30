7026dccf43eed75794c17bb80b1a95ba4e3663a9
amir <lara.programmer@gmail.com>
2024-09-30 20:19:00 +0330

    chore(subscription): add subscription model

---
+++ app/Models/Subscription.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Subscription extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'plan', 'starts_at', 'expires_at'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
